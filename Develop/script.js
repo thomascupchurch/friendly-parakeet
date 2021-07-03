25bcd9ea672185ac8bd112d342b957f5f2dbc6e7
Boba Fett <thomascupchurch@gmail.com>
2021-07-03 12:17:48 -0500

    add confirm statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+$( window ).on( "load", function() {
+
 
 // define variables and connect to Ids
 const resultEl = document.getElementById('password');
@@ -7,6 +9,54 @@ const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
+
+alert("Enter the number of characters you want in the box.");
+
+// use confirm boxes to ask what kind of characters user wants and save those choices to check boxes
+function askUserInput() {
+
+var upper = confirm("Do you want uppercase letters?");
+if (upper == true) {
+  alert("Your password will include uppercase letters.");
+  uppercaseEl.checked = true;
+} else {
+  alert("Your password will not include uppercase letters.");
+}
+
+
+var lower = confirm("Do you want lowercase letters?");
+if (lower == true) {
+  alert("Your password will include lowercase letters.");
+  lowercaseEl.checked = true;
+} else {
+  alert("Your password will not include lowercase letters.");
+}
+
+
+var numbers = confirm("Do you want numbers?");
+if (numbers == true) {
+  alert("Your password will include numbers.");
+  numbersEl.checked = true;
+} else {
+  alert("Your password will not include numbers.");
+}
+
+
+var symbols = confirm("Do you want special characters?");
+if (symbols == true) {
+  alert("Your password will include special characters.");
+  symbolsEl.checked = true;
+} else {
+  alert("Your password will not include special characters.");
+}
+
+if (lengthEl === null) {
+  alert("Enter a number from 8 through 128 in the box");
+}
+}
+
+askUserInput();
+
 
 
 // create master randomizer function 
@@ -18,72 +68,6 @@ const randomFunction = {
 };
 
 
-
-// create function to generate password
-function generatePassword(lower, upper, number, symbol, length) {
-  // initialize a password variable
-  let generatedPassword = '';
-
-  const typesCount = lower + upper + number + symbol;
-
-  console.log('typesCount: ', typesCount);
-  // convert list of types to an array and filter out those that are not checked
-
-
-  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
-  (
-    item => Object.values(item)[0]
-  );
-
-  console.log('typesArr: ', typesArr);
-
-  if (typesCount === 0) {
-    alert("Please choose at least one type of character.");
-    return '';
-  }
-
-  if (lengthEl.value < 8) {
-    alert("Please enter a number of characters from 8 through 128");
-    return '';
-  }
-
-  if (lengthEl.value > 128) {
-    alert("Please enter a number of characters from 8 through 128");
-    return '';
-  }
-
-  for (let i =0; i < length; i += typesCount) {
-    typesArr.forEach(type => {
-      const functionName = Object.keys(type)[0];
-      console.log('functionName: ', functionName);
-      generatedPassword += randomFunction[functionName]();
-    });
-  }
-  
-  const finalPassword = (generatedPassword.slice(0, length));
-  console.log(finalPassword);
-  return finalPassword;
-
-
-};
-
-
-// generate event listener
-generateEl.addEventListener('click', () => {
-  const length = +lengthEl.value;
-  const hasLower = lowercaseEl.checked;
-  const hasUpper = uppercaseEl.checked;
-  const hasNumbers = numbersEl.checked;
-  const hasSymbols = symbolsEl.checked;
-
-  resultEl.innerText = generatePassword(
-    hasLower, 
-    hasUpper, 
-    hasNumbers, 
-    hasSymbols, 
-    length
-    );
-});
 
 // functions for individual categories of characters
 function getRandomLower() {
@@ -107,3 +91,66 @@ function getRandomSymbol() {
 // console.log(getRandomUpper());
 // console.log(getRandomNumber());
 // console.log(getRandomSymbol());
+
+// event listener for generation of password
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumbers = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower, 
+    hasUpper, 
+    hasNumbers, 
+    hasSymbols, 
+    length
+    );
+});
+
+
+// create function to generate password
+function generatePassword(lower, upper, number, symbol, length) {
+  // initialize a password variable
+  let generatedPassword = '';
+  // save number of chosen character styles to a variable
+  const typesCount = lower + upper + number + symbol;
+
+  console.log('typesCount: ', typesCount);
+  
+  // convert list of types to an array and filter out those that are not checked
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
+  (
+    item => Object.values(item)[0]
+  );
+
+  console.log('typesArr: ', typesArr);
+  // require user to choose at least one type of character
+  if (typesCount === 0) {
+    alert("Please choose at least one type of character.");
+    return '';
+  }
+
+  // enforce min and max by giving user prompt 
+  if ((lengthEl.value < 8) || (lengthEl.value > 128)) {
+    alert("Please enter a number of characters from 8 through 128");
+    return '';
+  }
+
+  // for number of times equal to chosen password length, loop through function that randomly chooses from array of chosen types and saves result to generated password
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const functionName = Object.keys(type)[0];
+      console.log('functionName: ', functionName);
+      generatedPassword += randomFunction[functionName]();
+    });
+  }
+  
+  // save generated password to variable to be rendered on screen
+  const finalPassword = (generatedPassword);
+  console.log(finalPassword);
+  return finalPassword;
+}
+
+});
